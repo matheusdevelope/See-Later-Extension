1fcc3e2ccc6d0821e6ac27ce6680ac82e3222c1a
Matheus Teixeira <emersonmatheustu@gmail.com>
2021-09-20 07:55:45 -0400

    Commit test

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const ListaLocal = localStorage.getItem("ListaItem")
     const ListaExcludesLocal = localStorage.getItem("ListaExcludes")
